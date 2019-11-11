b7d1d1651c3e2a26ed76194cfbd26c5d47f029f5
simon <simon.jobin@heig-vd.ch>
2019-11-11 11:20:59 +0100

    basic sql schema to do tests...

--- images/mysql/data/affinitiesNetwork-schema.sql
+++ images/mysql/data/affinitiesNetwork-schema.sql
@@ -8,42 +8,52 @@ CREATE SCHEMA affinitiesNetwork;
 USE affinitiesNetwork;
 
 CREATE TABLE amt_user (
-  user_id INT,
+  user_id INT NOT NULL auto_increment,
   firstName VARCHAR(45),
   lastName VARCHAR(45),
   PRIMARY KEY(user_id)
 );
   
 CREATE TABLE amt_centerOfInterest (
-  coi_id INT,
+  coi_id INT NOT NULL auto_increment,
+  coi_name VARCHAR(45),
   description VARCHAR(45),
   PRIMARY KEY  (coi_id)
 );
 
 CREATE TABLE amt_affinity (
-	affinity_id int,
-    affinity_level int,
-    PRIMARY KEY (affinity_id)
+	affinity_id INT NOT NULL auto_increment,
+    affinity_level INT,
+    user_id INT,
+    coi_id INT,
+    PRIMARY KEY (affinity_id),
+    CONSTRAINT
+    FOREIGN KEY (user_id) REFERENCES amt_user(user_id),
+    FOREIGN KEY (coi_id) REFERENCES amt_centerOfInterest(coi_id)
 );
 
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (0, "Simon", "Jobin");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (1, "Toto", "Titi");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (2, "Pierre", "Dupont");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (3, "Georges", "Bush");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (4, "Gilles", "Surchat");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (5, "Pierre", "Dupond");
-INSERT INTO amt_user(user_id, firstName, lastName) VALUES (6, "Paul", "Dupond");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Simon", "Jobin");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Toto", "Titi");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Pierre", "Dupont");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Georges", "Bush");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Gilles", "Surchat");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Pierre", "Dupond");
+INSERT INTO amt_user(firstName, lastName) VALUES ("Paul", "Dupond");
 
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (0, "football");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (1, "handball");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (2, "guitare");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (3, "piano");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (4, "rando");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (5, "jeux videos");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (6, "dessin");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (7, "peinture");
-INSERT INTO amt_centerOfInterest(coi_id, description) VALUES (8, "harmonica");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("football", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("handball", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("guitare", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("piano", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("rando", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("jeux videos", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("dessin", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("peinture", "");
+INSERT INTO amt_centerOfInterest(coi_name, description) VALUES ("harmonica", "");
 
+INSERT INTO amt_affinity(affinity_level, user_id, coi_id) VALUES (12, 0, 3);
+INSERT INTO amt_affinity(affinity_level, user_id, coi_id) VALUES (8, 1, 3);
+INSERT INTO amt_affinity(affinity_level, user_id, coi_id) VALUES (4, 1, 4);
+INSERT INTO amt_affinity(affinity_level, user_id, coi_id) VALUES (5, 2, 6);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
